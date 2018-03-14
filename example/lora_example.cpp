fb0847043b6ce8f1f612dfd1060adf2758a09f04
Max <savagecm@users.noreply.github.com>
2018-03-14 09:21:25 +0800

    aa

--- example/lora_example.cpp
+++ example/lora_example.cpp
@@ -4,6 +4,7 @@
 int glob_fd;
 void tmp_cb(void)
 {
+    printf("in the tmp_cb function\n");
     int num = 0;
     num = serialDataAvail(glob_fd);
     if (num < 1)
@@ -23,11 +24,10 @@ void lora_server_example()
 
     auto server_ptr = lora_uart_server::instance();
 
-
     if (!server_ptr->init())
     {
         __LOG(error, "lora server init fail");
-        return ;
+        return;
     }
     else
     {
@@ -36,22 +36,22 @@ void lora_server_example()
 
     while (1)
     {
-        delay(10000);
+        
+        delay(100);
         __LOG(debug, "running.....");
 #if 0
         printf("receive %X\n", serialGetchar(glob_fd));
-
+#endif
 
         printf("now send message\n");
-        char tmp_buf[10] = {0x11, 0x12, 0x13};
+        char tmp_buf[10] = {'A', 'B', 'C'};
         server_ptr->send(tmp_buf, 3, 0x00, 0x1, 1);
-#endif
     }
 }
 
 void lora_client_example()
 {
-    lora_uart lora(0x3, 0x3, 12);
+    lora_uart lora(0x00, 0x01, 1);
     lora.set_low_power_call_back(tmp_cb);
     if (lora.init())
     {
@@ -60,15 +60,26 @@ void lora_client_example()
     {
         printf("lora init fail!");
     }
-    delay(1000);
-    while (1) //(serialDataAvail(glob_fd))
+    glob_fd = lora.get_fd();
+    delay(1000000000);
+#if 0
+    while (1)
     {
-        printf("receive %X\n", serialGetchar(glob_fd));
+        delay(100);
+        while (serialDataAvail(glob_fd))
+        {
+
+            printf("................");
+            printf("receive %c\n", serialGetchar(glob_fd));
+        }
     }
+
+
     while (1)
     {
         delay(1000);
         printf(".");
         //printf("receive %X\n", serialGetchar(lora.getfd()));
     }
+#endif
 }
